0f84d46ff8e73d53594cfb3a44e208581b6352a4
Ai-see-data <aishuk3@gmail.com>
2026-04-24 18:01:37 -0400

    Update main.py

--- news-app/main.py
+++ news-app/main.py
@@ -1,41 +1,64 @@
+import os
 import requests
-from send_email import send_email
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+from send_mail import send_email
+load_dotenv()
+
 
 topic = "AI OR artificial intelligence OR machine learning"
 sources = "techcrunch,the-verge,wired,ars-technica"
-api_key="85fbf459e010401a99c6dc767d4db97b"
-URL="https://newsapi.org/v2/everything?"\
+News_api_key=os.getenv("News_api_key")
+Gemini_api_key=os.getenv("Gemini_api_key")
+
+URL=("https://newsapi.org/v2/everything?"\
     f"sources={sources}&"\
     f"q={topic}&" \
     "sortBy=publishedAt&"\
     "language=en&"\
-    "apiKey=85fbf459e010401a99c6dc767d4db97b"
-
-
+    "pageSize=8&"\
+    + News_api_key
+)
 #make request
 request = requests.get(URL) #sends http get req to newsapi and newsapi prepares the response in JSON
-
 #get a dict w data
 content = request.json() # parses json data into something python can work with ( dict DS )
-#print(content)
-
-#print(type(content)) shows dict after including json - to extract data, we need a data structure
+#print(type(content)) o/p is 'dict' after including json() - to extract data, we need a data structure
+articles= content["articles"]
+#print(articles)
 #Access the article's content like title and desc
 #Build email body
-mail=" "
-for article in content["articles"][:20]:
-    if article['title'] is not None:
-        mail += f"{article['title']}\n"
-        mail += f"{article['description']}\n"
-        mail += f"{article['url']}\n"
-        mail += "-"*100 + "\n\n"
+def top_headlines():
+    mail=" "
+    for article in content["articles"][:20]:
+        if article['title'] is not None:
+            mail += f"{article['title']}\n"
+            mail += f"{article['description']}\n"
+            mail += f"{article['url']}\n"
+            mail += "-"*100 + "\n\n"
+#calling the custom send_email func        
+    send_email(subject="What's happening in AI today?", body=mail, to_email="aishuk3@gmail.com")
 
-#mail = mail.encode("utf-8")
-send_email(subject="What's happening in AI today?", body=mail, to_email="aishuk3@gmail.com")
+#instead of sending all the articles to the user, we're gonna have AI analyze the Json data in content & mail it
 
-#img="https://upload.wikimedia.org/wikipedia/commons/6/64/Dall-e_3_%28jan_%2724%29_artificial_intelligence_icon.png"
-#res=requests.get(img)
-#with open("img.jpg", 'wb') as file:
-    #file.write(res.content)
+#AI summarizing the news
+def headlines_summary():
 
+    model =init_chat_model(
+        model="gemini-3-flash-preview", 
+        model_provider="google-genai", 
+        api_key=Gemini_api_key
+    )
+    prompt = f"""You're a news summarizer.
+    Write two short paragraphs analyzing those news, aiding someone to go over the news during their morning coffee.
+    Here are the news articles: {articles}
+    """
+    response = model.invoke(prompt)
+    response_str=response.content[0]['text']
+
+    send_email(subject="Good Morning, Today's AI news summary", body=response_str, to_email="aishuk3@gmail.com") #calling the custom send_email func
+
+
+top_headlines()
+headlines_summary()
 
